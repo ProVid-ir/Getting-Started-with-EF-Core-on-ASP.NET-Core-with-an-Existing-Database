2a0fa77a019c91fa8ea489d1e197a20d01e32935
ProVid-ir <32803076+ProVid-ir@users.noreply.github.com>
2017-10-14 14:03:40 -0700

    Add files via upload

---
+++ EFGetStarted.AspNetCore.ExistingDb/Models/EFGetStarted_AspNetCore_NewDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Models
+{
+    public partial class EFGetStarted_AspNetCore_NewDbContext : DbContext
+    {
+        public virtual DbSet<Blogs> Blogs { get; set; }
+        public virtual DbSet<Posts> Posts { get; set; }
+
+        public EFGetStarted_AspNetCore_NewDbContext(DbContextOptions<EFGetStarted_AspNetCore_NewDbContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blogs>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+            });
+
+            modelBuilder.Entity<Posts>(entity =>
+            {
+                entity.HasKey(e => e.PostId);
+
+                entity.HasIndex(e => e.BlogId);
+
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.BlogId);
+            });
+        }
+    }
+}
